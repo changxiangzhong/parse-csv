4559dc108f338320ac844259e759d03054b74a52
Chang Xiangzhong <changxiangzhong@gmail.com>
2016-06-02 22:11:25 +0200

    Record column names & current line name

--- main.c
+++ main.c
@@ -6,28 +6,62 @@
 
 static const char DELIMITER = ';';
 
-typedef char (*column_name)[128];
+#define COLUMN_NAME_LEN 128
+typedef char (*column_name)[COLUMN_NAME_LEN];
 #define LINE_NAME_LEN 32
 typedef char (*line_name)[LINE_NAME_LEN];
 
 struct parse_context {
-    int y; // line index
-    int x; // column index
+    unsigned int y; // line index
+    unsigned int x; // column index
     line_name ln_name;
-    column_name *cl_name;
+    column_name *cl_names;
     size_t cl_name_len;
+    size_t cl_name_cap;
 };
 
 void init_parse_context(struct parse_context *context)
 {
+    size_t i = 0;
     context -> x = 0;
     context -> y = 0;
     context -> ln_name = calloc(sizeof(char), LINE_NAME_LEN);
+
+    context -> cl_name_len = 0;
+    // hard code the initial length to 32
+    context -> cl_name_cap = 32;
+    context -> cl_names = calloc(sizeof(column_name), context->cl_name_cap);
+
+    for (; i < context -> cl_name_cap; i++) {
+       context -> cl_names[i] = calloc(sizeof(char), COLUMN_NAME_LEN);
+    }
+}
+
+void expand_parse_contet_1st_column(struct parse_context *context)
+{
+    size_t i;
+    printf("expand_parse_contet_1st_column\n");
+    column_name* tmp = context -> cl_names;// save the temp pointer
+    context -> cl_name_cap *= 2;
+    context -> cl_names = calloc(sizeof(column_name), context->cl_name_cap);
+    memcpy(context -> cl_names, tmp, context -> cl_name_len * sizeof(column_name));
+
+    free(tmp);
+
+    for (i = context -> cl_name_len; i < context -> cl_name_cap; i++) {
+       context -> cl_names[i] = calloc(sizeof(char), COLUMN_NAME_LEN);
+    }
 }
 
 void free_parse_context(struct parse_context *context)
 {
+    size_t i = 0;
     free(context -> ln_name);
+
+    for (; i < context -> cl_name_cap; i++) {
+        free(context -> cl_names[i]);
+    }
+    free(context -> cl_names);
 }
 
 /**
@@ -82,19 +116,32 @@ void free_input_buf(char* buffer)
 void end_of_field_cb(void* buf, size_t len, void* context_data)
 {
     struct parse_context *context = (struct parse_context*) context_data;
-    if (context -> x == 0)
-        strncpy((char*)context -> ln_name, buf, len);
 
-    else 
+    if (context -> y == 0) {// Store the 1st column names
+        if (context -> x >= context -> cl_name_cap) {// expand it if necessary 
+            expand_parse_contet_1st_column(context);
+        }
+
+        // save the 1st column items as name
+        strncpy((char*) context -> cl_names[context -> cl_name_len++], buf, len);
+    }
+
+    if (context -> x == 0) {
+        strncpy((char*)context -> ln_name, buf, len);
+    } else {
         printf(
-            "@year=%s\tcell[%d][%s]=%*.*s\n", 
+            "@year=%s\t@company=%s\tcell[%d][%s]=%*.*s\n", 
             (char*)context -> ln_name,
+            (char*)context->cl_names[context->x],
             context -> y + 1, 
             to_alphabetic_column(context -> x), 
             (int)len, 
             (int)len, 
             (char*)buf
             );
+    }
+
+    
     context -> x++;
 }
 
